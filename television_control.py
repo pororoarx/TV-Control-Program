a2eb6884acf8f8b18b4bc212f03ded174e51b7ad
pororoarx <kengarrido.legaspi@gmail.com>
2023-05-16 21:51:28 +0800

    Pseudocode draft

---
+++ television_control.py
@@ -0,0 +1,33 @@
+# Start 
+
+# Create a class named TV
+    # Constructor
+
+    # On and Off control
+    # on control
+    # off control
+
+    # Get current channel 
+
+    # Channel control
+
+    # Channel up control
+
+    # Channel down control
+
+    # Get current volume
+
+    # Volume control
+
+    # Volume up control
+
+    # Volume down control
+
+# Create object for TV1
+# Print channel and volume of TV1
+
+# Create object for TV2
+# Print channel and volume of TV2
+    
+
+
